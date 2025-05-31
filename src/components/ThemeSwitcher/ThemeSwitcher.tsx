b964ab29fa4364dbe6551e396bf968bb7afb400e
Igor Phillipe <IgrPhillipe@gmail.com>
2025-05-31 20:42:44 -0300

    fix: hydration error

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,10 +1,19 @@
+'use client';
+
 import { MoonIcon } from '@/components/ui/moon';
 import { SunIcon } from '@/components/ui/sun';
 import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 import { SidebarButton } from '../Sidebar/SidebarButton';
 
 export const ThemeSwitcher = () => {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   const isDark = theme === 'dark';
 
   const handleThemeChange = () => {
@@ -16,6 +25,10 @@ export const ThemeSwitcher = () => {
   };
 
   const Icon = isDark ? SunIcon : MoonIcon;
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <SidebarButton name={isDark ? 'Claro' : 'Escuro'} icon={Icon} onClick={handleThemeChange} />
