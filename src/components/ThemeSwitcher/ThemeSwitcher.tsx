4f5727e10f2c3ddddbb449aec755e1c2f5d27647
Igor Phillipe <IgrPhillipe@gmail.com>
2025-05-31 20:45:25 -0300

    feat: change layout

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -3,16 +3,11 @@
 import { MoonIcon } from '@/components/ui/moon';
 import { SunIcon } from '@/components/ui/sun';
 import { useTheme } from 'next-themes';
-import { useEffect, useState } from 'react';
+import { Suspense } from 'react';
 import { SidebarButton } from '../Sidebar/SidebarButton';
 
 export const ThemeSwitcher = () => {
   const { theme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
 
   const isDark = theme === 'dark';
 
@@ -26,11 +21,9 @@ export const ThemeSwitcher = () => {
 
   const Icon = isDark ? SunIcon : MoonIcon;
 
-  if (!mounted) {
-    return null;
-  }
-
   return (
-    <SidebarButton name={isDark ? 'Claro' : 'Escuro'} icon={Icon} onClick={handleThemeChange} />
+    <Suspense fallback={<></>}>
+      <SidebarButton name={isDark ? 'Claro' : 'Escuro'} icon={Icon} onClick={handleThemeChange} />
+    </Suspense>
   );
 };
