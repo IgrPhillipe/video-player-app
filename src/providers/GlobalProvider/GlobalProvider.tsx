830f3d602e61fb3c5b50c202912f8243e3158015
Igor Phillipe <IgrPhillipe@gmail.com>
2025-05-30 21:31:02 -0300

    fix: build errors

--- src/providers/GlobalProvider/GlobalProvider.tsx
+++ src/providers/GlobalProvider/GlobalProvider.tsx
@@ -1,11 +1,8 @@
-import { SessionProvider } from 'next-auth/react';
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 
-import { auth } from '@/auth';
-
-import { TanstackProvider } from '../TanstackProvider';
-import { ThemeProvider } from '../ThemeProvider';
-import { TransitionProvider, TransitionVariants } from '../TransitionProvider';
+import { TanstackProvider } from "../TanstackProvider";
+import { ThemeProvider } from "../ThemeProvider";
+import { TransitionProvider, TransitionVariants } from "../TransitionProvider";
 
 type GlobalProviderProps = Readonly<{
   children: ReactNode;
@@ -14,31 +11,27 @@ type GlobalProviderProps = Readonly<{
 const transitionVariants: TransitionVariants = {
   enter: {
     opacity: 1,
-    transition: { duration: 0.8, ease: 'easeOut' },
+    transition: { duration: 0.8, ease: "easeOut" },
   },
   exit: {
     opacity: 0,
-    transition: { duration: 0.8, ease: 'easeIn' },
+    transition: { duration: 0.8, ease: "easeIn" },
   },
 };
 
 export const GlobalProvider = async ({ children }: GlobalProviderProps) => {
-  const session = await auth();
-
   return (
     <TransitionProvider variants={transitionVariants}>
-      <SessionProvider session={session}>
-        <TanstackProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="light"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
-        </TanstackProvider>
-      </SessionProvider>
+      <TanstackProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </TanstackProvider>
     </TransitionProvider>
   );
 };
